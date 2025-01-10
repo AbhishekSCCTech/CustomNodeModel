6a6c6ec0156cc5707676fff75a2bba9f99475f0a
Abhishek Suryawanshi <Abhishek Suryawanshi@DESKTOP-BLUISQN>
2025-01-10 20:03:59 +0530

    Add project files.

---
+++ CustomNodeModel/GridNodeView.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using Dynamo.Controls;
+using Dynamo.Wpf;
+
+namespace CustomNodeModel.CustomNodeModel
+{
+    public class CustomNodeModelView : INodeViewCustomization<GridNodeModel>
+    {
+        public void CustomizeView(GridNodeModel model, NodeView nodeView)
+        {
+            // Create the slider control
+          
+            // Bind the slider's Value property to the SliderValue property of the model
+            // Set the Binding using SetBinding for the value property
+            Binding binding = new Binding("SliderValue")
+            {
+                Source = model,
+                Mode = BindingMode.TwoWay // Ensure two-way binding
+            };
+
+            var customComboBox = new CustomComboBox();
+
+            // Add the slider to the input grid
+            nodeView.inputGrid.Children.Add(customComboBox);
+        }
+
+        public void Dispose()
+        {
+            // Any necessary cleanup
+        }
+    }
+}
